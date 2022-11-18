41d737efa705baf79af8c5105e943f4e14f3aa0b
kzrnm <gengesa@gmail.com>
2022-11-18 22:17:30 +0900

    If git doesn't exist, test_load_static_files is skipped.

--- tests/documents/test_builder.py
+++ tests/documents/test_builder.py
@@ -1,10 +1,16 @@
 from pathlib import Path
+import shutil
+
+import pytest
 
 from competitive_verifier import git
 from competitive_verifier.documents.builder import load_static_files
 from competitive_verifier.documents.type import SiteRenderConfig
 
 
+@pytest.mark.skipif(
+    shutil.which("git") is None, reason="docs subcommand requires git command"
+)
 def test_load_static_files():
     config = SiteRenderConfig(
         config_yml={"foo": 1},
