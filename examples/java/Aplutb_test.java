8342cfa9fbf9b81d15055cf0f3670dd8e3d5af87
kzrnm <gengesa@gmail.com>
2023-10-14 18:21:31 +0900

    Add aplusb

---
+++ examples/java/Aplutb_test.java
@@ -0,0 +1,11 @@
+// competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/aplusb
+package examples.java;
+
+public class Aplutb_test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
