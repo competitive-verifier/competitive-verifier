47930a9c351b53868d8a7e2e3cbc2446fa407205
kzrnm <gengesa@gmail.com>
2024-02-14 22:36:20 +0900

    Add examples

---
+++ examples/cpp/bit_minus.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <algorithm>
+
+int64_t bit_minus(int64_t a)
+{
+    return ~a + 1;
+}
---
+++ examples/cpp/bit_minus_random_test.cpp
@@ -0,0 +1,18 @@
+#define STANDALONE
+#include <iostream>
+#include <cassert>
+#include <random>
+#include "examples/cpp/bit_minus.hpp"
+using namespace std;
+
+int main()
+{
+    mt19937 rnd(random_device{}());
+    for (size_t i = 0; i < 100000; i++)
+    {
+        auto num = rnd();
+        cout << num << " " << bit_minus(num) << endl;
+        assert(-(int64_t)num == bit_minus(num));
+    }
+    return 0;
+}
