e2092b5e008e1ca57c3b472845e1605d54bc8ee3
AlfredChester <fredbao1126@gmail.com>
2025-04-04 19:38:27 +0800

    Formatted code.

--- src/competitive_verifier/oj/__init__.py
+++ src/competitive_verifier/oj/__init__.py
@@ -1,5 +1,5 @@
 from .tools.download_command import run_wrapper as download
-from .tools.func import get_cache_directory, get_directory, get_atcoder_cache_dir
+from .tools.func import get_atcoder_cache_dir, get_cache_directory, get_directory
 from .tools.test_command import check_gnu_time
 from .tools.test_command import run_wrapper as test
 
