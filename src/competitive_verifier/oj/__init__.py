ffa6f6a654f3a553fb8aebd95d93d68b89db4cd0
kzrnm <gengesa@gmail.com>
2025-12-23 19:53:19 +0900

    Rename oj commands

--- src/competitive_verifier/oj/__init__.py
+++ src/competitive_verifier/oj/__init__.py
@@ -1,7 +1,7 @@
-from .tools.download_command import run_wrapper as download
 from .tools.func import get_cache_directory, get_directory
-from .tools.test_command import check_gnu_time
-from .tools.test_command import run_wrapper as test
+from .tools.oj_download import run_wrapper as download
+from .tools.oj_test import check_gnu_time
+from .tools.oj_test import run_wrapper as test
 
 __all__ = [
     "check_gnu_time",
