2ce5ef1f8830d86a3779e7ebdab618e277252e26
kzrnm <gengesa@gmail.com>
2026-01-23 11:09:33 +0900

    tests_integration

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,14 +4,6 @@ import pytest
 from pytest_mock import MockerFixture
 
 
-def pytest_addoption(parser: pytest.Parser):
-    parser.addoption(
-        "--use-prev-dest",
-        action="store_true",
-        help="Skip deletion of dst_dir.",
-    )
-
-
 @pytest.fixture
 def mock_perf_counter(mocker: MockerFixture):
     pc = 0.0
--- tests_integration/conftest.py
+++ tests_integration/conftest.py
@@ -19,6 +19,19 @@ from .types import ConfigDirSetter, FilePaths
 from .utils import dummy_commit_time
 
 
+def pytest_addoption(parser: pytest.Parser):
+    parser.addoption(
+        "--use-prev-dest",
+        action="store_true",
+        help="Skip deletion of dst_dir.",
+    )
+
+
+@pytest.fixture
+def mockenv(mocker: MockerFixture, request: pytest.FixtureRequest):
+    mocker.patch.dict(os.environ, request.param or {}, clear=True)
+
+
 @pytest.fixture(scope="session")
 def check_necessary_commands() -> str | None:  # pragma: no cover
     git_path = shutil.which("git")
@@ -64,7 +77,7 @@ def additional_path(
 
 @pytest.fixture(scope="session")
 def file_paths(use_prev_result: bool) -> FilePaths:
-    root = pathlib.Path(__file__).parent.parent.parent / "integration_test_data"
+    root = pathlib.Path(__file__).parent.parent / "integration_test_data"
     dest_root = root / "dst_dir"
     assert root.exists()
 
