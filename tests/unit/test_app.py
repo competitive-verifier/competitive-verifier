6dfbd7b42d63b01cf11aaaa84404257c3cbae26d
kzrnm <gengesa@gmail.com>
2026-04-15 02:16:08 +0900

    Fix the download subcommand; to read the verify_json environment variable

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -263,7 +263,9 @@ test_parse_args_params: list[
             "subcommand": "download",
             "verbose": True,
             "urls": ["https://example.com/ex1", "https://example.com/ex2"],
-            "verify_files_json": None,
+            "verify_files_json": pathlib.Path(
+                ".competitive-verifier/verify_files.json"
+            ),
         },
     ),
     (
